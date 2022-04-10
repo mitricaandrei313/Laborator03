3e72e937089a8a91ceef80177dd94417aec7d44b
Mitrica Andrei <andrei_ioan.mitrica@cti.pub.ro>
2022-04-10 20:58:25 +0300

    initial commit

---
+++ app/src/main/java/lab03/eim/systems/cs/pub/phonedialer/MainActivity.java
@@ -0,0 +1,14 @@
+package lab03.eim.systems.cs.pub.phonedialer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
